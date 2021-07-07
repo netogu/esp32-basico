879c30e24825f3000dfbe647fa693f292814b126
Ernesto Gonzalez Urdaneta <eurdaneta@apple.com>
2021-07-07 12:33:11 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println('Hola Mundo');
+  delay(500);
+}
